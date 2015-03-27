f30d736843c9787b2cf0df6922032bec5049f708
Max Myalkin <maxmyalkin@gmail.com>
2015-03-27 05:41:25 +0300

    many tables with stubs

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+
+    var step = 0;
+
     var $quantitySubmit = $('#quantity_submit'),
-        $secondBlock = $('#second');
+        $secondBlock = $('#second'),
+        $nextMatrix = $('#next_matrix');
 
     var choiceNumber, criteriaNumber;
+
+    $nextMatrix.on('click', getNext);
 
     $quantitySubmit.on('click', function() {
         var choiceValue = parseInt($('#choices').val());
@@ -16,13 +22,30 @@ $(document).ready(function() {
         choiceNumber = choiceValue;
         criteriaNumber = criteriaValue;
         $secondBlock.removeAttr('hidden');
-
-        $secondBlock.append(getMatrix(5));
+        $nextMatrix.before(getMatrix(criteriaNumber, 'criteria', 'Сравнение критериев'));
 
     });
 
-    function getMatrix(size) {
-        var $table = $('<table/>', {'class': 'table table-hover table-bordered'});
+    function getNext() {
+        if(step < criteriaNumber + 1) {
+            $nextMatrix.before(getMatrix(choiceNumber, 'choice', 'Сравнение вариантов по критерию ' + step));
+
+        } else {
+            //конец матриц
+            $('#result').remove();
+            $nextMatrix.text("Обновить");
+            $nextMatrix.before($('<h1/>', {'id': 'result', 'text': 'THE END'}));
+        }
+    }
+
+
+    function getMatrix(size, type, caption) {
+        var $table = $('<table/>', {
+            'class': 'table table-hover table-bordered',
+            'data-type': type,
+            'data-step': step});
+
+        $table.append($('<caption/>', {'text': caption}));
         var $tHead = $('<thead/>');
         var $tBody = $('<tbody/>');
         var $headRow = $('<tr/>');
@@ -33,6 +56,8 @@ $(document).ready(function() {
         for(var i = 0; i < size; i++) {
             tds.push($('<td/>', {'text': i + 1}));
         }
+        tds.push($('<td/>', {'text': 'c'}));
+        tds.push($('<td/>', {'text': 'alpha'}));
 
         $headRow.append(tds);
 
@@ -43,12 +68,30 @@ $(document).ready(function() {
             for(var j = 0; j < size; j++) {
                 var $input;
                 if(i == j) {
-                    $input = $('<input/>', {'data-row': i, 'data-column': j, 'class':"form-control", 'value':"*", 'readonly': 'readonly', 'tabindex': -1});
+                    $input = $('<input/>', {
+                        'data-type': 'value',
+                        'data-row': i,
+                        'data-column': j,
+                        'class':"form-control",
+                        'value':"1",
+                        'readonly': 'readonly',
+                        'tabindex': -1});
                 } else {
                     if(i > j) {
-                        $input = $('<input/>', {'data-row': i, 'data-column': j, 'class':"form-control inp", 'readonly': 'readonly', 'tabindex': -1});
+                        $input = $('<input/>', {
+                            'data-type': 'value',
+                            'data-row': i,
+                            'data-column': j,
+                            'class':"form-control",
+                            'readonly': 'readonly',
+                            'tabindex': -1});
                     } else {
-                        $input = $('<input/>', {'data-row': i, 'data-column': j, 'class':"form-control inp", 'type':"number"});
+                        $input = $('<input/>', {
+                            'data-type': 'value',
+                            'data-row': i,
+                            'data-column': j,
+                            'class':"form-control",
+                            'type':"number"});
                         $input.keyup(function () {
                             this.value = this.value.replace(/[^0-9\.]/g,'');
                         });
@@ -68,6 +111,25 @@ $(document).ready(function() {
                 }
                 tds.push($('<td/>').append($input));
             }
+
+            tds.push($('<td/>')
+                .append($('<input/>', {
+                    'data-type': 'c',
+                    'data-step': step,
+                    'data-row': i,
+                    'class':"form-control",
+                    'readonly': 'readonly',
+                    'tabindex': -1})));
+
+            tds.push($('<td/>')
+                .append($('<input/>', {
+                    'data-type': 'alpha',
+                    'data-step': step,
+                    'data-row': i,
+                    'class':"form-control",
+                    'readonly': 'readonly',
+                    'tabindex': -1})));
+
             $tr.append(tds);
             trs.push($tr);
         }
@@ -75,6 +137,20 @@ $(document).ready(function() {
         $tBody.append(trs);
         $table.append([$tHead, $tBody]);
 
-        return $table;
+        var $matrix = $('<div/>', {'class': 'col-md-12'});
+        var $infoDiv = $('<div/>', {'class': 'row'});
+        $matrix.append([$table, $infoDiv]);
+        $infoDiv.append([
+            $('<p/>', {'class': 'col-md-5',
+                'text': 'статус',
+                'data-type': type,
+                'data-step': step}),
+            $('<button/>', {'text': 'Проверить',
+                class: 'pull-right btn btn-success',
+                'data-type': type,
+                'data-step': step})
+        ]);
+        step += 1;
+        return $matrix;
     }
 });
